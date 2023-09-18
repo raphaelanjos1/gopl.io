65fbd02516d214e464e6869cd7c5de718b76e7c8
raphaelanjos1 <raphael-anjos@live.com>
2023-09-18 00:45:05 -0300

    exercises 1.1 and 1.2

--- ch1/echo1/main.go
+++ ch1/echo1/main.go
@@ -12,13 +12,19 @@ import (
 	"os"
 )
 
+// func main() {
+// 	var s, sep string
+// 	for i := 0; i < len(os.Args); i++ {
+// 		s += sep + os.Args[i]
+// 		sep = " "
+// 	}
+// 	fmt.Println(s)
+// }
+
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
+	for i, arg := range os.Args {
+		fmt.Printf("%d: %s\n", i, arg)
 	}
-	fmt.Println(s)
 }
 
 //!-
